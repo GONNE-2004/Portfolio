059b5b5ab9139bdf6926e54e895d10490ac23e65
GONNE <gonne411@gmail.com>
2026-03-24 21:59:39 +0200

    added gonne.json but it's still incomplete

--- generate_portfolio.py
+++ generate_portfolio.py
@@ -4,8 +4,11 @@ from pathlib import Path
 
 from jinja2 import Environment, FileSystemLoader
 
+# Base directory (directory where this script is located)
+BASE_DIR = Path(__file__).parent
+
 # Load JSON data
-with Path("portfolio-gonne.json").open(encoding="utf-8") as f:
+with Path(BASE_DIR/"gonne.json").open(encoding="utf-8") as f:
     data = json.load(f)
 
 # Add any extra context if needed
@@ -14,11 +17,11 @@ data["current_year"] = datetime.now(tz=UTC).year
 if "social_links" in data:
     for link in data["social_links"]:
         if link.get("svg_path"):
-            with Path(link["svg_path"]).open(encoding="utf-8") as svg_file:
+            with Path(BASE_DIR/link["svg_path"]).open(encoding="utf-8") as svg_file:
                 link["svg_data"] = svg_file.read()
 
 # Set up Jinja environment
-env = Environment(loader=FileSystemLoader("."), autoescape=True)
+env = Environment(loader=FileSystemLoader(BASE_DIR), autoescape=True)
 index_template = env.get_template("index_template.html")
 resume_template = env.get_template("resume_template.html")
 
@@ -31,10 +34,10 @@ resume_output = resume_template.render(**data)
 # resume_output = resume_template.render(name=data["name"], label=data["label"]...)
 
 # Write the output to an HTML file
-with Path("index.html").open("w", encoding="utf-8") as f:
+with Path(BASE_DIR/"index.html").open("w", encoding="utf-8") as f:
     f.write(html_output)
 
-with Path("resume.html").open("w", encoding="utf-8") as f:
+with Path(BASE_DIR/"resume.html").open("w", encoding="utf-8") as f:
     f.write(resume_output)
 
 print("HTML file generated successfully!")
